3f55a0ee86393a2cd06f7add28055b9102eb3da0
cherukuri333 <teja.cherukuri3@gmail.com>
2025-04-02 15:47:38 +0530

    upadted the docker commnds

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull cherukurisai/buildcommands:v2
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 cherukurisai/buildcommands:v2
