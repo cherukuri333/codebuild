84bf5d3efbd253ee65b4cd413092def233ad8387
cherukuri333 <teja.cherukuri3@gmail.com>
2025-04-02 16:06:23 +0530

    run for cicd

--- start_container.sh
+++ start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from Docker Hub 
 docker pull cherukurisai/buildcommands:v2
 
 # Run the Docker image as a container
